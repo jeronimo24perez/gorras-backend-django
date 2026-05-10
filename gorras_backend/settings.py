df36ed1bf545fbffb125cbccbad70a72b15c212a
jeronimo24perez <jeronimo24perez@gmail.com>
2026-05-09 20:01:31 -0500

    settings

--- gorras_backend/settings.py
+++ gorras_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lx4^atg7-^a3moj46&8vm06267r2pb7age6u)sv+%4yo)v@p=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gorras-backend-django-1ui3.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
